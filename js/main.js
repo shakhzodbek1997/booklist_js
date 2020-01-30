fac6980a3143b43fdb4b3307130b9504ba2ea97b
Shakhzodbek Yuldoshev <shakhzodbek.yuldoshev@gmail.com>
2020-01-30 15:35:16 +0500

    delete function

--- js/main.js
+++ js/main.js
@@ -43,6 +43,13 @@ class UI {
         list.appendChild(row);
     }
 
+    static deleteBook(el){
+        if (el.classList.contains('delete')){
+            el.parentElement.parentElement.remove();
+        }
+    }
+
+
     static clearFields(){
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -77,4 +84,7 @@ document.querySelector('#book-form').addEventListener('submit', (e) =>
 });
 
 // Event: Remove a book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+   UI.deleteBook(e.target)
+});
 
